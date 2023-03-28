b5641fc3f88e724d3f00dd303373bea5e496c1d8
Kingsley Oyeoka <kingstoneoy@gmail.com>
2023-03-28 09:25:22 +0100

    Complete react front end

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import "./Button.css";
+import { Link } from 'react-router-dom';
 
 const Button = (props) => {
   return (
-    <div><button className={`btn ${props.type}`}><span className="btn-title">{props.text}</span></button></div>
+    <div><button className={`btn ${props.type}`}><Link to='/contact'><span className="btn-title">{props.text}</span></Link></button></div>
   )
 }
 
